5f5903d48bcfbb710f5dbcc63aebfd7db3c9942d
shaunlo <shaunlo60664@gmail.com>
2022-02-16 17:28:56 -0700

    Add project files.

---
+++ M7/Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace M7.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumBooks { get; set; }
+        public int BooksPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        //figure out how many pages we need
+        public int TotalPages => (int)Math.Ceiling((double)TotalNumBooks / BooksPerPage);
+    }
+}
